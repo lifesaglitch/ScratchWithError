ede0a460489445885c4c327db2d841ddadc68727
Huan Lin <linhuan@Huans-MacBook-Pro.local>
2013-06-09 22:16:05 +0800

    first commit

---
+++ ScratchWithError/AnotherViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnotherViewController.h
+//  ScratchWithError
+//
+//  Created by Huan Lin on 6/8/13.
+//  Copyright (c) 2013 Huan Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnotherViewController : UIViewController
+
+@end
